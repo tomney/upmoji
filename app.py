a0c891d1b9474613046d926442b93e8a61e5a697
Gregory Tomney <gregorytomney@Gregorys-MacBook-Pro.local>
2018-08-05 16:31:50 -0400

    introduce frontend/server architecture

--- app.py
+++ app.py
@@ -3,22 +3,28 @@ from redis import Redis, RedisError
 import os
 import socket
 
+# Set the directory path name as a local variable
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def main():
     try:
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+    html_file = open(_get_html_template("main.html"), 'r')
+    html = html_file.read()
+    
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+def _get_html_template(filename):
+    return os.path.join(ROOT_PATH, 'frontend', filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
