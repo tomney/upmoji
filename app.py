8e96b63599261be62595b1b17aa7ffa0a7027ea7
tomney <gregory.tomney@gmail.com>
2018-08-11 15:03:50 -0400

    structure app as flask app to serve templates and images

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from flask import Flask
-from redis import Redis, RedisError
+from flask import Flask, url_for, render_template
 import os
 import socket
 
 # Set the directory path name as a local variable
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+    first_image = "/static/images/Virginia.png"
+    second_image = "/static/images/Gonzaga.png"
 
-    html_file = open(_get_html_template("main.html"), 'r')
-    html = html_file.read()
-    
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
-
-def _get_html_template(filename):
-    return os.path.join(ROOT_PATH, 'frontend', filename)
+    return render_template('main.html', first_image=first_image, second_image=second_image)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
