1c212a5c899f071c24309c1a1b383c85d161fc7c
tomney <gregory.tomney@gmail.com>
2018-08-12 15:45:18 -0400

    randomize images

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask, url_for, render_template
 import os
 import socket
 
+from server import get_random_image_set
+
 # Set the directory path name as a local variable
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
+def _get_static_url(path):
+    # return os.path.relpath(path)
+    return path
+
 @app.route("/")
 def main():
-    first_image = "/static/images/Virginia.png"
-    second_image = "/static/images/Gonzaga.png"
-
+    first_image, second_image = get_random_image_set(2, [])
+    first_image = _get_static_url(first_image)
+    second_image = _get_static_url(second_image)
     return render_template('main.html', first_image=first_image, second_image=second_image)
 
 if __name__ == "__main__":
