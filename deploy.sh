102a0d9df599ccab2c3e43746a9976f5a15238a6
tomney <gregory.tomney@gmail.com>
2018-10-23 22:12:36 -0400

    add untracked bash script for deployment

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# Configure gcloud
+gcloud config set project upmoji
+gcloud config set compute/zone us-central1-a
+
+# Set environment variables
+export PROJECT_ID="$(gcloud config get-value project -q)"
+
+# Build and push container
+docker build -t gcr.io/${PROJECT_ID}/upmoji:gke .
+docker push gcr.io/${PROJECT_ID}/upmoji:gke
